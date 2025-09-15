db9837accc437cc2a64f1f5a0c1ab2aaa17197e0
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-09-15 14:22:06 +0200

    add(contact.cpp): Add full getter/setter + test

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -2,33 +2,79 @@
 #include "Contact.hpp"
 
 Contact::Contact(void) {
-	// Code du constructeur (peut être vide pour l'instant)
+	// Code du constructeur
 }
 
 Contact::~Contact(void) {
-	// Code du destructeur (peut être vide pour l'instant)
+	// Code du destructeur
 }
 
 
-
+// ------ setter ------ //
 
 void Contact::setFirstName(std::string firstName) {
 
 	this->firstName = firstName;
 }
 
-std::string Contact::getFirstName() const{
-	return firstName;
+void Contact::setName(std::string name) {
+
+	this->name = name;
+}
+
+void Contact::setNickName(std::string nickName) {
+	this->nickName = nickName;
+}
+
+void Contact::setPhoneNumber(std::string phoneNumber) {
+	this->phoneNumber = phoneNumber;
+}
+
+void Contact::setDarkestSecret(std::string darkestSecret) {
+	this->darkestSecret = darkestSecret;
 }
 
 
 
+// ------ getter ------ //
+
+std::string Contact::getFirstName() const {
+	return firstName;
+}
+
+std::string Contact::getName() const {
+	return name;
+}
+
+std::string Contact::getNickName() const {
+	return nickName;
+}
+
+std::string Contact::getPhoneNumber() const {
+	return phoneNumber;
+}
+
+std::string Contact::getDarkestSecret() const {
+	return darkestSecret;
+}
+
 
 int main()
 {
-	Contact contact;
-	contact.setFirstName("test");
-	std::cout << "name : " << contact.getFirstName() << std::endl;
+	Contact new_contact;
+	new_contact.setFirstName("boby");
+	new_contact.setName("shmeuda");
+	new_contact.setNickName("B-Boy");
+	new_contact.setPhoneNumber("07070707");
+	new_contact.setDarkestSecret("sniffeur2baltrou");
+
+
+	std::cout << "first name: " << new_contact.getFirstName() << std::endl;
+	std::cout << "name: " << new_contact.getName() << std::endl;
+	std::cout << "nick name: " << new_contact.getNickName() << std::endl;
+	std::cout << "phone number: " << new_contact.getPhoneNumber() << std::endl;
+	std::cout << "secret: " << new_contact.getDarkestSecret() << std::endl;
+
 	return 0;
 
 }
