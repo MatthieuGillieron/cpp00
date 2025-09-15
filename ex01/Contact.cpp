8010300cb77d3ecd56504135d7516bb39fddb88d
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-09-15 14:05:03 +0200

    add(contact.cpp): Implementation for getter and setter

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -1,3 +1,34 @@
 #include <iostream>
 #include "Contact.hpp"
 
+Contact::Contact(void) {
+	// Code du constructeur (peut être vide pour l'instant)
+}
+
+Contact::~Contact(void) {
+	// Code du destructeur (peut être vide pour l'instant)
+}
+
+
+
+
+void Contact::setFirstName(std::string firstName) {
+
+	this->firstName = firstName;
+}
+
+std::string Contact::getFirstName() const{
+	return firstName;
+}
+
+
+
+
+int main()
+{
+	Contact contact;
+	contact.setFirstName("test");
+	std::cout << "name : " << contact.getFirstName() << std::endl;
+	return 0;
+
+}
