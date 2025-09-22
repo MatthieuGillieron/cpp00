4a975a2044dcdbfeacff10e1003ebdbc345ff9a8
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-09-22 11:05:09 +0200

    add(main): Add loop for command + exec

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -10,19 +10,39 @@ std::string command;
 
 while (true) {
 
-	std::cout << "Enter command [ADD] [SEARCH] [EXIT]: ";
+	std::cout << "Enter command [ADD] [SEARCH] [EXIT]: " << std::endl;
 	std::cin >> command;
 
 
 	if (command == "ADD") {
 
+		std::string firstName, lastName, nickName, phoneNumber, darkestSecret;
+
+		std::cout << "Enter first name: ";
+		std::cin >> firstName;
+
+		std::cout << "Enter last name: ";
+		std::cin >> lastName;
+
+		std::cout << "Enter nick name: ";
+		std::cin >> nickName;
+
+		std::cout << "Enter phone number: ";
+		std::cin >> phoneNumber;
+
+		std::cout << "Enter darkest secret: ";
+		std::cin >> darkestSecret;
 
 
+		phoneBook.addContact(firstName, lastName, nickName, phoneNumber, darkestSecret);
+
+		std::cout << "Contacrt added !" << std::endl;
 	}
+
 
 	else if (command == "SEARCH") {
 
-
+		phoneBook.searchContact();
 
 	}
 	else if (command == "EXIT")
