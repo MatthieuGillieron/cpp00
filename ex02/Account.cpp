a8ec87307e11b8ac0455361a420abba66a24c9a7
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-09-23 09:56:12 +0200

    add: methode get

---
+++ ex02/Account.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/22 15:11:42 by mg                #+#    #+#             */
+/*   Updated: 2025/09/23 09:55:58 by mg               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account(void) {}
+
+
+
+int	Account::getNbAccounts(void) {
+	return _nbAccounts;
+}
+
+int Account::getTotalAmount(void) {
+	return _totalAmount;
+}
+
+int Account::getNbDeposits(void) {
+	return _totalNbDeposits;
+}
+
+int Account::getNbWithdrawals(void) {
+	return _totalNbWithdrawals;
+}
